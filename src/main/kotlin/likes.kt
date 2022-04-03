19cd28335c55f1bbbf457624d841d9781e8637c9
lenochka69 <93248482+lenochka69@users.noreply.github.com>
2022-04-04 00:12:37 +0300

    Like

---
+++ src/main/kotlin/likes.kt
@@ -0,0 +1,11 @@
+fun incrementAndPrint(likes: Int): Int {
+    val likes = 4
+    val newLikes = likes + 1
+    println(newLikes)
+    return newLikes
+
+}
+
+fun main() {
+    incrementAndPrint(2)
+}
