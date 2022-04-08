fc9571c6deaef182ebaae6c371683210a518b62c
lenochka69 <93248482+lenochka69@users.noreply.github.com>
2022-04-08 22:10:21 +0300

    Like

--- src/main/kotlin/likes.kt
+++ src/main/kotlin/likes.kt
@@ -1,11 +1,8 @@
-fun incrementAndPrint(likes: Int): Int {
-    val likes = 4
-    val newLikes = likes + 1
-    println(newLikes)
-    return newLikes
-
-}
-
 fun main() {
-    incrementAndPrint(2)
+    val likes = 1
+    val likesLength = likes.toString().length
+    val penultimateSymbol = if (likesLength > 1) likes.toString()[likesLength - 2] else '0'
+    val ending = if (likes % 10 == 1 && penultimateSymbol != '1') "человеку" else "людям"
+
+    println("Понравилось: $likes $ending")
 }
